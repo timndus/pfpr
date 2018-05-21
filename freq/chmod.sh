8ca3b3a853ab0f19696e6d67f77265f33ded9979
timndus <TIMNDUS>
2018-05-21 18:14:11 +0000

    Mon May 21 18:14:11 UTC 2018

--- freq/chmod.sh
+++ freq/chmod.sh
@@ -1,3 +1,4 @@
+chmod 444 /sys/devices/system/cpu/cpu*/cpufreq/cpuinfo_cur_freq
 chmod 646 /sys/devices/system/cpu/cpu*/cpufreq/scaling_max_freq
 chmod 646 /sys/devices/system/cpu/cpu*/cpufreq/scaling_min_freq
 chmod 646 /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
