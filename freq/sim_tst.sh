e492a5ce5980b403692f11f1c11a46f9336a8fd2
Ali Moradi Alamdarloo <timndus@gmail.com>
2018-07-06 20:34:11 +0430

    sim_tst

---
+++ freq/sim_tst.sh
@@ -0,0 +1,40 @@
+for ((  i=0; i<$1; i++  ))
+do
+	./set_to_all.sh 1600000
+	sleep 10
+
+	./set_to_all.sh 1600000
+	sleep 10
+	
+	./set_to_all.sh 1700000
+	sleep 5
+
+	./set_freq.sh 1 1800000
+	sleep 5
+
+
+
+
+	./set_to_all.sh 1600000
+	sleep 10
+	
+	./set_to_all.sh 1700000
+	sleep 5
+
+	./set_freq.sh 0 1600000
+	./set_freq.sh 1 1900000
+	sleep 5
+
+	
+
+	./set_to_all.sh 1600000
+	sleep 10
+
+	./set_to_all.sh 1700000
+	sleep 5
+
+	./set_freq.sh 0 1600000
+	./set_freq.sh 1 1800000
+	./set_freq.sh 2 1800000
+	sleep 5
+done
