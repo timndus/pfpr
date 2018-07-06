0fb6db3a00ac4f5af8dcb78f268c511912f64a95
Ali Moradi Alamdarloo <timndus@gmail.com>
2018-07-06 21:00:39 +0430

    sim_tst_v1

---
+++ rapl/log/sim_tst/sim_tst_v1.py
@@ -0,0 +1,26 @@
+# Import the necessary packages and modules
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+
+y1 = []
+
+with open('sim_tst_v1.log','r') as csvfile:
+	plots = csv.reader(csvfile, delimiter='\n')
+	for row in plots:
+	    y1.append(float(row[0]))
+
+
+plt.plot(y1, label='sim_tst_v1')
+
+plt.xlabel('Sample number')
+plt.ylabel('Power(W)')
+
+## setting the limits on the x-axis and y-axis
+#plt.xlim(1,20)
+#plt.ylim(0.6,1)
+
+plt.title('')
+plt.legend()
+#plt.savefig('me.png', dpi=500)
+plt.show()
