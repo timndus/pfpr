805dbaf5a152a836895575b5e11c452a03909505
Ali Moradi Alamdarloo <timndus@gmail.com>
2018-07-16 19:47:26 +0430

    exp_stress_all_limit_v2

--- mpich_benchmark/exp_stress_all_limit.sh
+++ mpich_benchmark/exp_stress_all_limit.sh
@@ -10,7 +10,7 @@ done
 
 echo "tst4_mode1"
 for (( i = 0; i < 5; i++ )); do
-	~/code/freq/./set_freq.sh 0 1800000
+	~/code/freq/./set_freq.sh 0 2500000
 	mpiexec -bind-to user:0,1,2,3 -n 4 ~/code/mpich_benchmark/./stress_all_limit | tail -n 4 | tee -a stress_all_limit.log
 	~/code/freq/./set_to_all.sh 1700000
 	sleep 10
@@ -18,8 +18,8 @@ done
 
 echo "tst4_mode2"
 for (( i = 0; i < 5; i++ )); do
-	~/code/freq/./set_freq.sh 0 1800000
-	~/code/freq/./set_freq.sh 1 1800000
+	~/code/freq/./set_freq.sh 0 2500000
+	~/code/freq/./set_freq.sh 1 2500000
 	mpiexec -bind-to user:0,1,2,3 -n 4 ~/code/mpich_benchmark/./stress_all_limit | tail -n 4 | tee -a stress_all_limit.log
 	~/code/freq/./set_to_all.sh 1700000
 	sleep 10
@@ -27,9 +27,9 @@ done
 
 echo "tst4_mode3"
 for (( i = 0; i < 5; i++ )); do
-	~/code/freq/./set_freq.sh 0 1800000
-	~/code/freq/./set_freq.sh 1 1800000
-	~/code/freq/./set_freq.sh 2 1800000
+	~/code/freq/./set_freq.sh 0 2500000
+	~/code/freq/./set_freq.sh 1 2500000
+	~/code/freq/./set_freq.sh 2 2500000
 	mpiexec -bind-to user:0,1,2,3 -n 4 ~/code/mpich_benchmark/./stress_all_limit | tail -n 4 | tee -a stress_all_limit.log
 	~/code/freq/./set_to_all.sh 1700000
 	sleep 10
@@ -37,10 +37,10 @@ done
 
 echo "tst4_mode4"
 for (( i = 0; i < 5; i++ )); do
-	~/code/freq/./set_freq.sh 0 1800000
-	~/code/freq/./set_freq.sh 1 1800000
-	~/code/freq/./set_freq.sh 2 1800000
-	~/code/freq/./set_freq.sh 3 1800000
+	~/code/freq/./set_freq.sh 0 2500000
+	~/code/freq/./set_freq.sh 1 2500000
+	~/code/freq/./set_freq.sh 2 2500000
+	~/code/freq/./set_freq.sh 3 2500000
 	mpiexec -bind-to user:0,1,2,3 -n 4 ~/code/mpich_benchmark/./stress_all_limit | tail -n 4 | tee -a stress_all_limit.log
 	~/code/freq/./set_to_all.sh 1700000
 	sleep 10
