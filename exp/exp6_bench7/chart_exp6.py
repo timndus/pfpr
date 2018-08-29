2e438afdd48effa1a5d669148d4552f3b123bdca
Ali Moradi Alamdarloo <timndus@gmail.com>
2018-08-30 01:50:35 +0430

    chart_exp6_default_nemesis_sock_vs_pfpr_msg_on_off

--- exp/exp6_bench7/chart_exp6.py
+++ exp/exp6_bench7/chart_exp6.py
@@ -5,22 +5,41 @@ import csv
 
 y1 = []
 y2 = []
+y3 = []
+y4 = []
 
-with open('default_nemesis.log','r') as csvfile:
+with open('default_nemesis_max_scaling_17.log','r') as csvfile:
     plots = csv.reader(csvfile, delimiter='\n')
     for row in plots:
         y1.append(float(row[0]))
 
-with open('default_sock.log','r') as csvfile:
+with open('default_sock_max_scaling_17.log','r') as csvfile:
     plots = csv.reader(csvfile, delimiter='\n')
     for row in plots:
         y2.append(float(row[0]))
+
+with open('pfpr_msg_on_nemesis_17_12.log','r') as csvfile:
+    plots = csv.reader(csvfile, delimiter='\n')
+    for row in plots:
+        y3.append(float(row[0]))
+
+with open('pfpr_msg_off_nemesis_17_12.log','r') as csvfile:
+    plots = csv.reader(csvfile, delimiter='\n')
+    for row in plots:
+        y4.append(float(row[0]))
 
 plt.plot(y1, label='default_nemesis')
 plt.plot(y1, 'o')
 
 plt.plot(y2, label='default_sock')
 plt.plot(y2, 'o')
+
+plt.plot(y3, label='pfpr_msg_on')
+plt.plot(y3, 'o')
+
+plt.plot(y4, label='pfpr_msg_off')
+plt.plot(y4, 'o')
+
 
 plt.xlabel('x label')
 plt.ylabel('ET')
