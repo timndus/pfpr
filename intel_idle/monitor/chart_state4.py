2f8a79c0c41a20ca769a5c895f938a6112e56d8c
Ali Moradi Alamdarloo <timndus@gmail.com>
2018-09-15 18:42:12 +0430

    Sat Sep 15 18:42:12 +0430 2018

---
+++ intel_idle/monitor/chart_state4.py
@@ -0,0 +1,61 @@
+# Import the necessary packages and modules
+import glob, os
+
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+
+y0 = []
+y1 = []
+y2 = []
+y3 = []
+y4 = []
+y5 = []
+
+with open('0state4.log','r') as csvfile:
+    plots = csv.reader(csvfile, delimiter='\n')
+    for row in plots:
+        y0.append(float(row[0]))
+
+with open('1state4.log','r') as csvfile:
+    plots = csv.reader(csvfile, delimiter='\n')
+    for row in plots:
+        y1.append(float(row[0]))
+
+with open('2state4.log','r') as csvfile:
+	plots = csv.reader(csvfile, delimiter='\n')
+	for row in plots:
+	    y2.append(float(row[0]))
+
+with open('3state4.log','r') as csvfile:
+	plots = csv.reader(csvfile, delimiter='\n')
+	for row in plots:
+	    y3.append(float(row[0]))
+
+with open('4state4.log','r') as csvfile:
+	plots = csv.reader(csvfile, delimiter='\n')
+	for row in plots:
+	    y4.append(float(row[0]))
+
+with open('5state4.log','r') as csvfile:
+	plots = csv.reader(csvfile, delimiter='\n')
+	for row in plots:
+	    y5.append(float(row[0]))
+
+plt.plot(y0, label='0state4')
+plt.plot(y1, label='1state4')
+plt.plot(y2, label='2state4')
+plt.plot(y3, label='3state4')
+plt.plot(y4, label='4state4')
+plt.plot(y5, label='5state4')
+
+plt.xlabel('Sample Number')
+plt.ylabel('Usage')
+ax.grid(which='both')
+#plt.xticks(np.arange(0, 20, 3))
+#plt.yticks(np.arange(65, 86, 5))
+plt.legend()
+#plt.savefig('pfpr_nemesis_17_12_execution_time.png', dpi=500)
+plt.show()
