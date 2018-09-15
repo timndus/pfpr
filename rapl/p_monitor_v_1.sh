f63ee1db1bbe4248b950b4e500ea581fb1d15961
timndus <timndus@gmail.com>
2018-09-15 11:27:54 +0430

    p_monitor_v_1

--- rapl/p_monitor_v_1.sh
+++ rapl/p_monitor_v_1.sh
@@ -2,7 +2,7 @@
 #executable sample_number log_file_name
 for (( j=0; j<$1; j++ ))
 do
-	var=$(~/code/rapl/e_read_v_1 | awk 'NR==15 {print $3}')
+	var=$(~/msc_project/rapl/e_read_v_1 | awk 'NR==14 {print $3}')
 	echo ${var%?} | tee -a $2.log
 	#echo ${var%?} >> $2.log
 done
