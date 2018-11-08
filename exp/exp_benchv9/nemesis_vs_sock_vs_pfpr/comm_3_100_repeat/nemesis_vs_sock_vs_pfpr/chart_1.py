e786d581b125afa4c11225e108e03a237098845f
Ali Moradi Alamdarloo <timndus@gmail.com>
2018-11-08 20:43:15 +0330

    chart_bench_v9_nemesis_vs_sock_vs_pfpr

---
+++ exp/exp_benchv9/nemesis_vs_sock_vs_pfpr/comm_3_100_repeat/nemesis_vs_sock_vs_pfpr/chart_1.py
@@ -0,0 +1,39 @@
+# Import the necessary packages and modules
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+
+y1 = []
+y2 = []
+y3 = []
+
+with open('nemesis/nemesis_1.log','r') as csvfile:
+    plots = csv.reader(csvfile, delimiter='\n')
+    for row in plots:
+        y1.append(float(row[0]))
+
+
+with open('sock/sock_1.log','r') as csvfile:
+    plots = csv.reader(csvfile, delimiter='\n')
+    for row in plots:
+        y2.append(float(row[0]))
+
+with open('pfpr/pfpr_1.log','r') as csvfile:
+    plots = csv.reader(csvfile, delimiter='\n')
+    for row in plots:
+        y3.append(float(row[0]))
+
+plt.plot(y1, label='default_nemesis_repeat_1')
+plt.plot(y2, label='default_sock_repeat_1')
+plt.plot(y3, label='pfpr_nemesis_repeat_1')
+
+plt.xlabel('Sample Number')
+plt.ylabel('Execution Time (sec)')
+ax.grid(which='both')
+#plt.xticks(np.arange(0, 20, 3))
+#plt.yticks(np.arange(18.48, 18.49, 0.001))
+plt.legend()
+plt.savefig('default_nemesis_vs_sock_vs_pfpr_repeat_1.png', dpi=500)
+plt.show()
